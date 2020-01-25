1782c9759ee36155ddda24dd405018e489f40fdf
FranciscoReyes19 <francisco.reyesfrt@outlook.com>
2020-01-25 15:16:18 -0600

    Mensaje de confirmacion Git

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost:27017/api_rest_node', {useNewUrlParser: true})
 	.then(() => {
-		console.log('La conexion a la DB mongo se ha realizado OK');
+		console.log('La conexion a la DB mongo se ha realizado OK_cambio en Git');
 		//crear el servidor
 		app.listen(port, () => {
 			console.log('el servidor esta run OK en por 39999 woooooowwwiii');
